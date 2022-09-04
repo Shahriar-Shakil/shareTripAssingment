f2d37f5d7a1d1788106301c1a2e789309ea92a25
mrshahriar <toshahriarshakil@gmail.com>
2022-09-04 19:41:34 +0600

    content group btn added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,62 @@
 import * as React from "react";
 
-import { Container } from "@mui/material";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Container,
+  Typography,
+  ToggleButtonGroup,
+  ToggleButton,
+  styled,
+} from "@mui/material";
 import Grid from "@mui/material/Grid";
 import BoxUI from "../src/Components/UI/BoxUI";
 import Layout from "../src/layout/Layout";
 import Sidebar from "../src/Components/Sidebar";
+import { useTypography } from "../src/stylesHook/stylesHook";
+import DiamondIcon from "@mui/icons-material/Diamond";
+import BoltIcon from "@mui/icons-material/Bolt";
+import SavingsIcon from "@mui/icons-material/Savings";
+
+const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({ theme }) => ({
+  "& .MuiToggleButtonGroup-grouped": {
+    // margin: theme.spacing(0),
+    border: 0,
+  },
+  "& .MuiToggleButton-root": {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "start",
+    flex: 1,
+    fontSize: "14px",
+    fontWeight: "500",
+    lineHight: "20.02px",
+    color: "#000",
+    textTransform: "none",
+    margin: 0,
+    padding: "10px 16px",
+    "& svg": {
+      float: "left",
+      fontSize: "30px",
+      marginRight: "8px",
+    },
+  },
+  ".Mui-selected": {
+    backgroundColor: "#3884F7 !important",
+    color: "#fff !important",
+    "&:hover": {
+      backgroundColor: "rgb(21, 101, 192) !important",
+    },
+  },
+}));
 
 export default function Index() {
+  const classes = useTypography({ fontSize: "16px" });
+  const [toggle, setToggle] = React.useState("2");
+  const handleToggle = (event, value) => {
+    setToggle(value);
+  };
   return (
     <Layout>
       <Container style={{ paddingTop: "14px" }} maxWidth="lg">
@@ -15,7 +65,42 @@ export default function Index() {
             <Sidebar />
           </Grid>
           <Grid item lg={9}>
-            <BoxUI padding={16}>sdsdsd</BoxUI>
+            <Box>
+              <Typography
+                className={classes.titleLg}
+                variant="body1"
+                color="initial"
+              >
+                48 Available Buses
+              </Typography>
+              <BoxUI light={true} sx={{ mt: 2 }}>
+                <StyledToggleButtonGroup
+                  value={toggle}
+                  exclusive
+                  onChange={handleToggle}
+                  sx={{ width: "100%" }}
+                  size="large"
+                  color="primary"
+                >
+                  <ToggleButton value="1">
+                    <DiamondIcon />
+                    Earliest Buses
+                  </ToggleButton>
+                  <ToggleButton value="2">
+                    <SavingsIcon />
+                    Cheapest Buses
+                  </ToggleButton>
+                  <ToggleButton value="3">
+                    <BoltIcon />
+                    Available Seats
+                  </ToggleButton>
+                  <ToggleButton value="4">
+                    <BoltIcon />
+                    Fastest Trips
+                  </ToggleButton>
+                </StyledToggleButtonGroup>
+              </BoxUI>
+            </Box>
           </Grid>
         </Grid>
       </Container>
--- src/Components/UI/BoxUI.jsx
+++ src/Components/UI/BoxUI.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { styled } from "@mui/material";
 
-const BoxWrapper = styled("div")(({ theme, padding }) => {
+const BoxWrapper = styled("div")(({ theme, padding, light }) => {
   return {
     padding: `${padding ? padding + "px" : "0px"}`,
-    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.15)",
+    boxShadow: `${
+      light
+        ? "0px 2px 4px rgba(0, 0, 0, 0.12)"
+        : "0px 4px 4px rgba(0, 0, 0, 0.15)"
+    }`,
     borderRadius: "6px",
     border: "1px solid #F6F6F6",
   };
