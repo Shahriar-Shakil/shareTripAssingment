311099b510bd2a3fa9173a3f7578bb678aff5d9a
mrshahriar <toshahriarshakil@gmail.com>
2022-09-05 03:57:07 +0600

    ticket body added

--- src/Components/TicketsUI.jsx
+++ src/Components/TicketsUI.jsx
@@ -25,8 +25,50 @@ export const useTicketStyles = makeStyles((theme) => {
       fontSize: "18px",
       fontWeight: 600,
     },
+    label: {
+      fontWeight: 400,
+      fontSize: "12px",
+      lineHeight: "15px",
+    },
+    location: {
+      fontWeight: 400,
+      fontSize: "16px",
+      lineHeight: "24px",
+      textTransform: "uppercase",
+    },
+    time: {
+      fontWeight: 600,
+      fontSize: "16px",
+      lineHeight: "24px",
+    },
+    counter: {
+      fontWeight: 500,
+      fontSize: "14px",
+      lineHeight: "18px",
+    },
+    date: {
+      fontWeight: 400,
+      fontSize: "12px",
+      lineHeight: "15px",
+    },
+    busIcon: {
+      position: "relative",
+      "&:before": {
+        content: 'url("/icons/arrowLeftPart.png")',
+        position: "absolute",
+        top: "-4px",
+        left: " 8px",
+      },
+      "&:after": {
+        content: 'url("/icons/arrowRightPart.png")',
+        position: "absolute",
+        top: "0px",
+        left: "133px",
+      },
+    },
   };
 });
+
 export default function TicketsUI() {
   const classes = useTypography({ fontSize: "16px" });
   const ticketStyles = useTicketStyles();
@@ -35,62 +77,118 @@ export default function TicketsUI() {
       <Grid container>
         <Grid item lg={9}>
           {/* heading start */}
-
           <Box sx={{ px: 3, py: 2 }}>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "start",
-              }}
-            >
-              <Typography
-                className={(classes.titleLg, classes.fontWeight600)}
-                component="p"
-              >
-                AC
-              </Typography>
-              <Box>
-                <Typography
-                  className={(classes.titleL, ticketStyles.title)}
-                  component="p"
-                >
-                  Hanif Enterprize
-                </Typography>
-                <Typography
-                  sx={{ fontSize: "10px", textAlign: "center" }}
-                  className={classes.titleXs}
-                  component="p"
-                >
-                  31-DHK-CTG(D)
-                </Typography>
-              </Box>
+            <Stack spacing={2}>
               <Box
                 sx={{
                   display: "flex",
-                  alignItems: "center",
+                  justifyContent: "space-between",
+                  alignItems: "start",
                 }}
               >
-                <CurrencyLiraIcon
-                  sx={(theme) => ({
-                    fontSize: "26px",
-                    background: theme.palette.warning.main,
-                    color: theme.palette.primary.darkYellow,
-                    borderRadius: "50%",
-                    mr: 1,
-                  })}
-                  color="warning"
-                />
                 <Typography
-                  sx={{ float: "right" }}
                   className={(classes.titleLg, classes.fontWeight600)}
                   component="p"
                 >
-                  50
+                  AC
                 </Typography>
+                <Box>
+                  <Typography
+                    className={(classes.titleL, ticketStyles.title)}
+                    component="p"
+                  >
+                    Hanif Enterprize
+                  </Typography>
+                  <Typography
+                    sx={{ fontSize: "10px", textAlign: "center" }}
+                    className={classes.titleXs}
+                    component="p"
+                  >
+                    31-DHK-CTG(D)
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <CurrencyLiraIcon
+                    sx={(theme) => ({
+                      fontSize: "26px",
+                      background: theme.palette.warning.main,
+                      color: theme.palette.primary.darkYellow,
+                      borderRadius: "50%",
+                      mr: 1,
+                    })}
+                    color="warning"
+                  />
+                  <Typography
+                    sx={{ float: "right" }}
+                    className={(classes.titleLg, classes.fontWeight600)}
+                    component="p"
+                  >
+                    50
+                  </Typography>
+                </Box>
               </Box>
-            </Box>
-            {/* heading end */}
+              {/* heading end */}
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Box sx={{ flex: 1 }}>
+                  <Typography className={ticketStyles.label} component="p">
+                    Form
+                  </Typography>
+                  <Typography className={ticketStyles.location} component="p">
+                    Dhaka
+                  </Typography>
+                  <Typography className={ticketStyles.time} component="p">
+                    11:30 PM
+                  </Typography>
+                  <Typography className={ticketStyles.counter} component="p">
+                    Kolabagan Counter
+                  </Typography>
+                  <Typography className={ticketStyles.date} component="p">
+                    22 June 2021
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{ flex: 1, textAlign: "center" }}
+                  className={ticketStyles.busIcon}
+                >
+                  <img src={"/icons/bus.png"} />
+                  <Typography
+                    sx={(theme) => ({
+                      color: theme.palette.warning.main,
+                    })}
+                    className={ticketStyles.counter}
+                    component="p"
+                  >
+                    16 seat available
+                  </Typography>
+                  <Typography className={ticketStyles.label} component="p">
+                    6h 00m
+                  </Typography>
+                </Box>
+                <Box sx={{ flex: 1, pl: 3 }}>
+                  <Typography className={ticketStyles.label} component="p">
+                    To
+                  </Typography>
+                  <Typography className={ticketStyles.location} component="p">
+                    Chottogram
+                  </Typography>
+                  <Typography className={ticketStyles.time} component="p">
+                    05:30 AM
+                  </Typography>
+                  <Typography className={ticketStyles.counter} component="p">
+                    Dampara Counter
+                  </Typography>
+                  <Typography className={ticketStyles.date} component="p">
+                    23 June 2021
+                  </Typography>
+                </Box>
+              </Box>
+            </Stack>
+            {/* ticket body end */}
           </Box>
         </Grid>
         <Grid sx={{ background: "blue" }} item lg={3}>
